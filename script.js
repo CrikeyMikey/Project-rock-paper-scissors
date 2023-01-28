742ccf4b6615565edeadc71c5b7d4ccf93cc21c7
Aydan Hoover <hooveraydan@gmail.com>
2023-01-27 17:04:24 -0800

    Edited JS file comment

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-//create function getComputerChoice() {
-    //use a variable to hold a number randomly choose a number between 0-3
-    //if number is equal to 1 
-        //return "rock"
-    //if numbber is equal to 2 
-        //return "paper"
-    //if number is equal to 3 
-        //return "scissors"
-    //else return NaN
-//}
-
+//Generates a random number between 1-3 and chooses a RPS choice based on the number
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     //A switch case statement could probably be used here as well but an if statement came to mind first
