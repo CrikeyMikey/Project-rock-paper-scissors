8a4165cb4b1e6657b51668afded4c224ea192857
Aydan Hoover <hooveraydan@gmail.com>
2023-01-30 20:38:36 -0800

    Added playround function

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 //Generates the computers choice using random number generation
-function getComputerChoice() {
+function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     //A switch case statement could probably be used here as well but an if statement came to mind first
-    if (randomNumber == 1) {
+    if (randomNumber === 1) {
         return "ROCK";
-    } else if (randomNumber == 2) {
+    } else if (randomNumber === 2) {
         return "PAPER";
-    } else if (randomNumber == 3) {
+    } else if (randomNumber === 3) {
         return "SCISSORS";
-    } else {
-        return NaN;
     }
 }
 
-//Used to log the players choice of either rock, paper, or scissors
-let input = prompt("Enter your choice here");
-
-function getPlayerChoice() {
-    return toUpperCase(input);
+//Plays a round of Rock, Paper, Scissors.
+function playRound(playerSelection, computerSelection) {
+    //if statement that will compare the choices given by the player and CPU
+    if (playerSelection === computerSelection) {
+       return "It's a Tie!";
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "You Win! rock beats scissors";
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You Win! Paper beats rock";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "You Win! Scissors beats paper";
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
 }
 
+
+const playerSelection = prompt("Pick from ROCK, PAPER, or SCISSORS");
+const computerSelection = computerChoice();
+console.log(playRound(playerSelection, computerSelection));
