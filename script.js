de0f5f8540cd82a5ae8567a86dabc7b5e0d529e6
Aydan Hoover <hooveraydan@gmail.com>
2023-02-23 19:58:50 -0800

    Refactored game logic, added styles, HTML elements

--- script.js
+++ script.js
@@ -1,80 +1,85 @@
-//Generates the computers choice using random number generation
-function computerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    //A switch case statement could probably be used here as well but an if statement came to mind first
-    if (randomNumber === 1) {
-        return "ROCK";
-    } else if (randomNumber === 2) {
-        return "PAPER";
-    } else if (randomNumber === 3) {
-        return "SCISSORS";
-    }
-}
-
-//Initializes a prompt in the browser to get the users choice
-function playerChoice() {
-    let choice = prompt("Choose from rock, paper, or scissors");
-    return choice.toUpperCase();
-}
-
-//variables for the score of the player and the computer, plus the round number
 let playerScore = 0;
 let computerScore = 0;
 
-//Plays a round of Rock, Paper, Scissors, calls the choice variables and runs them through a gauntlet of if statements
-function playRound() {
-    const computerSelection = computerChoice();
-    const playerSelection = playerChoice();
-    //if statement that will compare the choices given by the player and CPU
-    if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        //in each statement the score and round number is updated accordingly
-        computerScore++;
-        round++;
-        console.log("You Lose! rock beats paper");
-    }
-    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        playerScore++;
-        round++;
-        console.log("You win! rock beats scissors");
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-        playerScore++;
-        round++;
-        console.log("You win! paper beats rock");
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
-        computerScore++;
-        round++;
-        console.log("You Lose! scissors beats paper");
-    } 
-    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
-        computerScore++;
-        round++;
-        console.log("You Lose! rock beats scissors");
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-        playerScore++;
-        round++;
-        console.log("You win! scissors beats paper");
-    } else {
-        console.log("This round is a Tie!");
+const rockButton = document.querySelector('.rock')
+const paperButton = document.querySelector('.paper')
+const scissorsButton = document.querySelector('.scissors')
+const resultDiv = document.querySelector('.result')
+
+const computerChoice = () => {
+    const arrChoices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * arrChoices.length);
+    return arrChoices[randomNumber];
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    console.log('1 ', playerSelection, '2 ', computerSelection)
+    if (playerSelection === computerSelection) {
+        const p = document.createElement('p')
+        p.innerText = `You tied! You both picked ${playerSelection}`
+        resultDiv.appendChild(p);
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Lose! rock beats scissors'
+        resultDiv.appendChild(p);
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Win! scissors beats paper'
+        resultDiv.appendChild(p);
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Lose! Paper beats rock'
+        resultDiv.appendChild(p);
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Won! rock beats scissors'
+        resultDiv.appendChild(p);
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Lose! scissors beats paper'
+        resultDiv.appendChild(p);
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Won! paper beats rock'
+        resultDiv.appendChild(p);
     }
 }
 
-//primary game function which will run through the round function until either the player or computer score reaches 5
-function game() {
-    while (playerScore < 5 || computerScore < 5) {
-        //calls the round function which gathers the player and CPU choices
-        playRound();
-        //if the scores of either the player or CPU reach 5 halt execution and display one of 2 messages
-        if (playerScore >= 5) {
-            return("You Win! you have beaten the machine!");
-        }
-        else if (computerScore >= 5) {
-            return("You Lose! the machine won");
-        }
-        else {
-            console.log("Continue Playing");
-        }
-    }
-}
+rockButton.addEventListener('click', () => {
+    const computerSelection = computerChoice()
+    const playerSelection = 'rock'
+    playRound(playerSelection, computerSelection)
+})
+
+paperButton.addEventListener('click', () => {
+    const computerSelection = computerChoice();
+    const playerSelection = 'paper';
+    playRound(playerSelection, computerSelection);
+})
+
+scissorsButton.addEventListener('click', () => {
+    const computerSelection = computerChoice();
+    const playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection);
+})
+
+//const game = () => {
+//    while (playerScore < 5 || computerScore < 5) {
+//        playRound();
+//        if (playerScore >= 5) {
+//            return("You Win! you have beaten the machine!");
+//        }
+//        else if (computerScore >= 5) {
+//            return("You Lose! the machine won");
+//        }
+//        else {
+//            console.log("Continue Playing");
+//        }
+//    }
+//}
